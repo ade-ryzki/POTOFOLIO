dbf909d79ffc82e5591625bce66ca7dccf323d1e
miq28 <92211442+miq28@users.noreply.github.com>
2022-03-08 00:50:37 +0700

    update

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -11,6 +11,8 @@ export default function Post({post}) {
         setlike(isLiked ? like -1 : like+1)
         setIsLiked(!isLiked)
     }
+
+    
   return (
     <div className="post" >
         <div className="postWrapper">
