dda4d213475a734be7c38a0c4e5bff2e20b773ce
rrb13 <ars.roybudiman@gmail.com>
2022-03-09 00:07:21 +0800

    update data di Homepage

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -10,29 +10,29 @@ import { toastError } from "../../redux/actions/toastActions"
 
 
 export default function Rightbar() {
-  const[collections, setCollections] = useState([])
+  const[userCollections, setUserCollections] = useState([])
   const dispatch = useDispatch
 
-  // useEffect(() => {
-  //   fetchDataAllUser()
-  // })
+  useEffect(() => {
+    fetchDataAllUser()
+  })
 
-  // const fetchDataAllUser = async () => {
-  //   try{
-  //     const res = await axios.get(`${URL_API}/users?skip=0&take=5`)
-  //     const userAll = res.data.result
+  const fetchDataAllUser = async () => {
+    try{
+      const res = await axios.get(`${URL_API}/users?skip=0&take=5`)
+      const userAll = res.data.result
 
-  //     console.log(userAll)
-  //     setCollections(userAll)
-  //   } catch (error) {
-  //     if (error.response) {
-  //       dispatch(toastError(`${error.response.data.message}`))
-  //       console.log(error.response.data.message)
-  //     } else {
-  //       console.log(`Error`, error.message)
-  //     }
-  //   }
-  // }
+      console.log(userAll)
+      setUserCollections(userAll)
+    } catch (error) {
+      if (error.response) {
+        dispatch(toastError(`${error.response.data.message}`))
+        console.log(error.response.data.message)
+      } else {
+        console.log(`Error`, error.message)
+      }
+    }
+  }
 
   const HomeRightbar = () => {
     return (
@@ -46,17 +46,17 @@ export default function Rightbar() {
         <h2 className="rightbarSeeAll">See All</h2>
         <hr className="rightbarHr" />
         <ul className="rightbarUserList">
-          {/* {collections.map((u) => (
+          {userCollections.map((u) => (
             <User key={u.id} user={u} />
-          ))} */}
+          ))}
         </ul>
         <h4 className="rightbarTitle">LIST CONTEST</h4>
         <h2 className="rightbarSeeAll">See All</h2>
         <hr className="rightbarHr" />
         <ul className="rightbarUserList">
-          {/* {Contests.map((u) => (
+          {Contests.map((u) => (
             <ListContest key={u.id} contest={u} />
-          ))} */}
+          ))}
         </ul>
         
         
